3aa350324167977d558705ca1d8414dfb093cab5
Student9227 <153178343+PHCASCStudent@users.noreply.github.com>
2025-05-30 11:55:16 +0000

    Finished project

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import sys
 import os
-# --- os.environ['SDL_AUDIODRIVER'] = 'dummy' ---
+os.environ['SDL_AUDIODRIVER'] = 'dummy'
 pygame.init()
 
 WIDTH = 800
@@ -22,10 +22,8 @@ BALL_SIZE = 10
 PADDLE_SPEED = 7
 BALL_SPEED = 4
 
-POWERUP_SIZE = 20
-POWERUP_SPAWN_RATE = 0.001  # Much less frequent
-POWERUP_DURATION = 5000
-POWERUP_SIZE = 32  # Increased from 20
+POWERUP_SPAWN_RATE = 0.01  # Much less frequent
+POWERUP_SIZE = 40  # Increased from 20
 
 fireball_trail = []  # List of (x, y, timestamp)
 FIREBALL_DOT_LIFETIME = 500
@@ -45,18 +43,17 @@ ball_speed_y = BALL_SPEED * random.choice((1, -1))
 
 powerups = []
 fireball_active = False
-fireball_timer = 0
-speed_modifier = 1.0
-paddle_size_modifier = 1.0
-powerup_timer = 0
-active_powerup = None  
 powerup_effect_duration = 20000  
-powerup_on_screen = False
 
-cpu_miss_chance = 0.1
+active_powerups = {}  # {type: [expiration_times]}
+paddle_size_multiplier = 1.0
+speed_multiplier = 1.0
+cpu_speed_multiplier = 1.0
+fireball_active = False
+
+cpu_miss_chance = 0.0
 cpu_miss_increment = 0.01 
-cpu_miss_max = 0.50  
-cpu_speed_modifier = 1.0
+cpu_miss_max = 0.35  
 
 player_score = 0
 cpu_score = 0
@@ -65,18 +62,12 @@ big_font = pygame.font.Font(None, 72)
 
 level_select_cursor = 0  # For keyboard navigation
 
-def create_glow_surface(size, color, alpha=128):
-    surf = pygame.Surface((size * 3, size * 3), pygame.SRCALPHA)
-    pygame.draw.circle(surf, (*color, alpha), (size * 1.5, size * 1.5), size * 1.5)
-    return surf
-
 class Powerup:
     def __init__(self, x, y, type):
         self.rect = pygame.Rect(x, y, POWERUP_SIZE, POWERUP_SIZE)
         self.type = type
 
 def spawn_powerup():
-    global powerup_on_screen
     if len(powerups) < 2 and random.random() < POWERUP_SPAWN_RATE:
         x = random.randint(100, WIDTH - 100)
         y = random.randint(100, HEIGHT - 100)
@@ -85,29 +76,15 @@ def spawn_powerup():
         powerups.append(Powerup(x, y, powerup_type))
 
 def apply_powerup(powerup):
-    global fireball_active, fireball_timer, speed_modifier, paddle_size_modifier, powerup_timer, active_powerup
-    powerup_timer = pygame.time.get_ticks()
-    active_powerup = powerup.type
+    global fireball_active, fireball_timer
+    now = pygame.time.get_ticks()
+    duration = powerup_effect_duration
+    if powerup.type not in active_powerups:
+        active_powerups[powerup.type] = []
+    active_powerups[powerup.type].append(now + duration)
     if powerup.type == "fireball":
         fireball_active = True
-        fireball_timer = pygame.time.get_ticks()
-    elif powerup.type == "speed_modifier":
-        speed_modifier = 1.2
-        cpu_speed_modifier = 0.8
-    elif powerup.type == "paddle_size":
-        paddle_size_modifier = 1.5
-        player_paddle.height = int(PADDLE_HEIGHT * paddle_size_modifier)
-        cpu_paddle.height = int(PADDLE_HEIGHT * 0.7)
-        player_paddle.y = max(0, min(player_paddle.y, HEIGHT - player_paddle.height))
-        cpu_paddle.y = max(0, min(cpu_paddle.y, HEIGHT - cpu_paddle.height))
-
-def reset_powerups():
-    global fireball_active, speed_modifier, paddle_size_modifier
-    fireball_active = False
-    speed_modifier = 1.0
-    paddle_size_modifier = 1.0
-    player_paddle.height = PADDLE_HEIGHT
-    player_paddle.y = HEIGHT // 2 - PADDLE_HEIGHT // 2
+        fireball_timer = now
 
 def draw_level_select():
     screen.fill(BLACK)
@@ -225,11 +202,11 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 paused = not paused
-            if event.key == pygame.K_f and not active_powerup:
+            if event.key == pygame.K_f:
                 apply_powerup(Powerup(ball.x, ball.y, "fireball"))
-            if event.key == pygame.K_e and not active_powerup:
+            if event.key == pygame.K_e:
                 apply_powerup(Powerup(ball.x, ball.y, "paddle_size"))
-            if event.key == pygame.K_s and not active_powerup:
+            if event.key == pygame.K_s:
                 apply_powerup(Powerup(ball.x, ball.y, "speed_modifier"))
 
     if paused:
@@ -247,11 +224,11 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] and player_paddle.top > 0:
-        player_paddle.y -= int(PADDLE_SPEED * speed_modifier)
+        player_paddle.y -= int(PADDLE_SPEED * speed_multiplier)
     if keys[pygame.K_DOWN] and player_paddle.bottom < HEIGHT:
-        player_paddle.y += int(PADDLE_SPEED * speed_modifier)
+        player_paddle.y += int(PADDLE_SPEED * speed_multiplier)
 
-    cpu_speed = PADDLE_SPEED * 0.8 * cpu_speed_modifier
+    cpu_speed = PADDLE_SPEED * 0.8 * cpu_speed_multiplier
     if cpu_paddle.centery < ball.centery and cpu_paddle.bottom < HEIGHT:
         if ball_speed_x > 0 and random.random() < cpu_miss_chance:
             pass  
@@ -263,10 +240,40 @@ while running:
         else:
             cpu_paddle.y -= cpu_speed
 
-    current_speed_x = ball_speed_x * speed_modifier
-    current_speed_y = ball_speed_y * speed_modifier
+    current_speed_x = ball_speed_x * speed_multiplier
+    current_speed_y = ball_speed_y * speed_multiplier
     ball.x += current_speed_x
     ball.y += current_speed_y
+
+    now = pygame.time.get_ticks()
+    # Remove expired powerups and recalculate multipliers
+    to_remove = []
+    paddle_size_multiplier = 1.0
+    speed_multiplier = 1.0
+    cpu_speed_multiplier = 1.0
+    fireball_active = False
+    for ptype, expiries in active_powerups.items():
+        # Remove expired
+        expiries = [t for t in expiries if t > now]
+        if expiries:
+            active_powerups[ptype] = expiries
+            if ptype == "paddle_size":
+                paddle_size_multiplier = 1.0 + 0.5 * len(expiries)
+            elif ptype == "speed_modifier":
+                speed_multiplier = 1.0 + 0.2 * len(expiries)
+                cpu_speed_multiplier = max(0.2, 1.0 - 0.2 * len(expiries))
+            elif ptype == "fireball":
+                fireball_active = True
+        else:
+            to_remove.append(ptype)
+
+    player_paddle.height = int(PADDLE_HEIGHT * paddle_size_multiplier)
+    player_paddle.y = max(0, min(player_paddle.y, HEIGHT - player_paddle.height))
+    cpu_paddle.height = int(PADDLE_HEIGHT * (1.0 - 0.3 * len(active_powerups.get("paddle_size", []))))
+    cpu_paddle.y = max(0, min(cpu_paddle.y, HEIGHT - cpu_paddle.height))
+
+    for ptype in to_remove:
+        del active_powerups[ptype]
 
     # Fireball trail logic: add dot after moving ball
     if fireball_active:
@@ -333,21 +340,6 @@ while running:
         if ball.colliderect(powerup.rect):
             apply_powerup(powerup)
             powerups.remove(powerup)
-            powerup_on_screen = False
-
-    current_time = pygame.time.get_ticks()
-    # Reset powerups and clear fireball trail when effect ends
-    if active_powerup and current_time - powerup_timer > powerup_effect_duration:
-        fireball_active = False
-        speed_modifier = 1.0
-        cpu_speed_modifier = 1.0
-        paddle_size_modifier = 1.0
-        player_paddle.height = PADDLE_HEIGHT
-        cpu_paddle.height = PADDLE_HEIGHT
-        player_paddle.y = max(0, min(player_paddle.y, HEIGHT - player_paddle.height))
-        cpu_paddle.y = max(0, min(cpu_paddle.y, HEIGHT - cpu_paddle.height))
-        fireball_trail.clear()
-        active_powerup = None
 
     screen.fill(BLACK)
     pygame.draw.rect(screen, NEON_BLUE, player_paddle)
@@ -376,18 +368,21 @@ while running:
             pygame.draw.rect(screen, (0, 255, 255), powerup.rect)
 
     player_text = font.render(str(player_score), True, WHITE)
-    if active_powerup == "fireball":
-        powerup_text = font.render("FIREBALL", True, NEON_RED)
-    elif active_powerup == "speed_modifier":
-        powerup_text = font.render("SPEED UP", True, (0, 255, 0))
-    elif active_powerup == "paddle_size":
-        powerup_text = font.render("PADDLE SIZE", True, (0, 255, 255))
-    else:
-        powerup_text = font.render("", True, WHITE)
-    screen.blit(powerup_text, (WIDTH // 2 - powerup_text.get_width() // 2, 20))
+    powerup_texts = []
+    if "fireball" in active_powerups:
+        powerup_texts.append("FIREBALL")
+    if "speed_modifier" in active_powerups:
+        powerup_texts.append("SPEED UP x" + str(len(active_powerups["speed_modifier"])))
+    if "paddle_size" in active_powerups:
+        powerup_texts.append("PADDLE SIZE x" + str(len(active_powerups["paddle_size"])))
+
+    powerup_text = font.render(" | ".join(powerup_texts), True, NEON_RED if "fireball" in active_powerups else WHITE)
+    screen.blit(powerup_text, (WIDTH // 2 - powerup_text.get_width() // 2, HEIGHT - powerup_text.get_height() - 20))
     cpu_text = font.render(str(cpu_score), True, WHITE)
     screen.blit(player_text, (WIDTH // 4, 20))
     screen.blit(cpu_text, (3 * WIDTH // 4, 20))
+
+    
 
     pygame.display.flip()
     clock.tick(60)
