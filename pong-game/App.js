13817f0dd21c771713221b59d4a5a402566df829
Student9227 <153178343+PHCASCStudent@users.noreply.github.com>
2025-06-01 14:07:21 +0000

    Made changes, added Admob banner ad

--- pong-game/App.js
+++ pong-game/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { WebView } from 'react-native-webview';
-import * as FileSystem from 'expo-file-system';
-import { Asset } from 'expo-asset';
-import * as ScreenOrientation from 'expo-screen-orientation';
+import React, { useEffect, useState } from "react";
+import { WebView } from "react-native-webview";
+import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { AdMobBanner } from "expo-ads-admob";
+import { Platform } from "react-native";
 
 const App = () => {
   const [html, setHtml] = useState(null);
-
+  const BANNER_AD_UNIT_ID = Platform.select({
+    android: "ca-app-pub-5295742012044906/9653265755",
+  });
   useEffect(() => {
-    // Lock orientation to landscape
     ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
 
     (async () => {
-      const asset = Asset.fromModule(require('./assets/index.html'));
+      const asset = Asset.fromModule(require("./assets/index.html"));
       await asset.downloadAsync();
       const htmlString = await FileSystem.readAsStringAsync(asset.localUri);
       setHtml(htmlString);
@@ -22,11 +25,15 @@ const App = () => {
   if (!html) return null;
 
   return (
-    <WebView
-      originWhitelist={['*']}
-      source={{ html }}
-      style={{ flex: 1 }}
-    />
+    <>
+      <WebView originWhitelist={["*"]} source={{ html }} style={{ flex: 1 }} />
+      <AdMobBanner
+        bannerSize="smartBannerPortrait"
+        adUnitID={BANNER_AD_UNIT_ID}
+        servePersonalizedAds // true or false
+        onDidFailToReceiveAdWithError={(error) => console.log(error)}
+      />
+    </>
   );
 };
 
